f41aef30e61b725a7ab3e27d9b151c1fc7049dff
Aeron01 <one.soulreaper@gmail.com>
2021-04-09 15:59:06 +0200

    update

--- my-wget.js
+++ my-wget.js
@@ -1,15 +1,15 @@
 const fsPromises = require('fs/promises')
 const axios = require('axios')
 
-if (process.argv.length !== 4) {
-  console.log('Usage: node my-wget.js url dst.html')
+if (process.argv.length !== 3) {
+  console.log('Usage: node my-wget.js url')
   process.exit(1)
 }
 
 const main = async () => {
   try {
     const tmpHtml = await axios.get(process.argv[2])
-    await fsPromises.writeFile(process.argv[3], tmpHtml.data)
+    await fsPromises.writeFile('index.html', tmpHtml.data)
   } catch (e) {
     console.log(e.code)
   }
