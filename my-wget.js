5515e766b2ed29295a8e0cda7e476af14c84b221
Aeron01 <one.soulreaper@gmail.com>
2021-04-11 15:49:36 +0200

    finale update and is good now !

--- my-wget.js
+++ my-wget.js
@@ -10,11 +10,11 @@ const main = async () => {
   try {
     const tmpHtml = await axios.get(process.argv[2])
     await fsPromises.writeFile('index.html', tmpHtml.data)
-    const stats = await fsPromises.stat('index.html')
     console.log(tmpHtml.headers)
+    const stats = await fsPromises.stat('index.html')
     console.log(`Download file completed.\n file size ${stats.size} bytes`)
   } catch (e) {
-    console.log(e)
+    console.log(e.message)
   }
 }
 
